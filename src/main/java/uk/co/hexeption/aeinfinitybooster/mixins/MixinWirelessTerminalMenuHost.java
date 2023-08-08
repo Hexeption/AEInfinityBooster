5a329335f1c02f40b27a122566fd136c2124ee7b
Keir Davis <minecraftfun201@gmail.com>
2023-08-08 16:25:39 +0100

    Updated to Latest AE

--- src/main/java/uk/co/hexeption/aeinfinitybooster/mixins/MixinWirelessTerminalMenuHost.java
+++ src/main/java/uk/co/hexeption/aeinfinitybooster/mixins/MixinWirelessTerminalMenuHost.java
@@ -2,14 +2,17 @@ package uk.co.hexeption.aeinfinitybooster.mixins;
 
 import appeng.api.implementations.blockentities.IWirelessAccessPoint;
 import appeng.api.implementations.menuobjects.ItemMenuHost;
+import appeng.api.networking.IGrid;
 import appeng.blockentity.networking.WirelessAccessPointBlockEntity;
 import appeng.helpers.WirelessTerminalMenuHost;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import uk.co.hexeption.aeinfinitybooster.setup.ModItems;
 
@@ -25,18 +28,20 @@ public class MixinWirelessTerminalMenuHost extends ItemMenuHost {
     @Shadow
     private double currentDistanceFromGrid;
 
+    @Shadow @Final private IGrid targetGrid;
+
     public MixinWirelessTerminalMenuHost(Player player, int slot, ItemStack itemStack) {
         super(player, slot, itemStack);
     }
 
-    @Inject(method = "testWap", at = @At("HEAD"), cancellable = true)
-    private void testWap(IWirelessAccessPoint wirelessAccessPoint, CallbackInfoReturnable<Boolean> cir) {
+    @Inject(method = "getWapSqDistance", at = @At("HEAD"), cancellable = true)
+    private void testWap(IWirelessAccessPoint wirelessAccessPoint, CallbackInfoReturnable<Double> cir) {
 
         wirelessAccessPoint.getGrid().getMachines(WirelessAccessPointBlockEntity.class).forEach(wirelessBlockEntity -> {
 
             if (wirelessBlockEntity.getInternalInventory().getStackInSlot(0).is(ModItems.DIMENSION_CARD.get())) {
                 currentDistanceFromGrid = 32;
-                cir.setReturnValue(true);
+                cir.setReturnValue(Double.MAX_VALUE / 2);
             }
 
             if (!this.getPlayer().level().dimension().location().toString().equals(wirelessAccessPoint.getLocation().getLevel().dimension().location().toString())) {
@@ -45,9 +50,23 @@ public class MixinWirelessTerminalMenuHost extends ItemMenuHost {
 
             if (wirelessBlockEntity.getInternalInventory().getStackInSlot(0).is(ModItems.INFINITY_CARD.get())) {
                 currentDistanceFromGrid = 16;
-                cir.setReturnValue(true);
+                cir.setReturnValue(Double.MAX_VALUE / 2);
             }
         });
+    }
+
+    // Make sure we don't use more power than we should
+    @Redirect(method = "extractAEPower", at = @At(value = "INVOKE", target = "Ljava/lang/Math;min(DD)D"))
+    private double testPowerMultiplier(double a, double b) {
+        for (var wap : this.targetGrid.getMachines(WirelessAccessPointBlockEntity.class)) {
+            if (wap.getInternalInventory().getStackInSlot(0).is(ModItems.INFINITY_CARD.get())) {
+                return 16;
+            }
+            if (wap.getInternalInventory().getStackInSlot(0).is(ModItems.DIMENSION_CARD.get())) {
+                return 32;
+            }
+        }
+        return Math.min(a, b);
     }
 
 }
